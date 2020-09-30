a6f5117be12bfffc449c5c7ed66da42161400324
fazlikuzhan <fzlkzhn46@gmail.com>
2020-09-30 12:12:46 -0700

    feature ilk commit

--- src/test/java/BranchDeneme.java
+++ src/test/java/BranchDeneme.java
@@ -2,7 +2,8 @@ public class BranchDeneme {
 
     public static void main(String[] args) {
 
-
+        System.out.println("feature 1. satir");
+        System.out.println("feature 2. satir");
 
 
     }}
